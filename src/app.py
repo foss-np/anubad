17a3e16744fdcc365801d10b94529561ec367e97
rho <rho.rhoit@gmail.com>
2016-04-19 02:11:35 +0545

    enable signal handeling #6

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
+import signal
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
 
 from main import *
 
@@ -112,6 +113,47 @@ def argparser():
     return parser.parse_args()
 
 
+def InitSignal(gui):
+    def signal_action(signal):
+        if signal is 1:
+            print("Caught signal SIGHUP(1)")
+        elif signal is 2:
+            print("Caught signal SIGINT(2)")
+        elif signal is 15:
+            print("Caught signal SIGTERM(15)")
+        # gui.cleanup()
+        # TODO clean quit
+        Gtk.main_quit()
+
+    def idle_handler(*args):
+        print("Python signal handler activated.")
+        GLib.idle_add(signal_action, priority=GLib.PRIORITY_HIGH)
+
+    def handler(*args):
+        print("GLib signal handler activated.")
+        signal_action(args[0])
+
+    def install_glib_handler(sig):
+        unix_signal_add = None
+
+        if hasattr(GLib, "unix_signal_add"):
+            unix_signal_add = GLib.unix_signal_add
+        elif hasattr(GLib, "unix_signal_add_full"):
+            unix_signal_add = GLib.unix_signal_add_full
+
+        if unix_signal_add:
+            print("Register GLib signal handler: %r" % sig)
+            unix_signal_add(GLib.PRIORITY_HIGH, sig, handler, sig)
+        else:
+            print("Can't install GLib signal handler, too old gi.")
+
+    SIGS = [getattr(signal, s, None) for s in "SIGINT SIGTERM SIGHUP".split()]
+    for sig in filter(None, SIGS):
+        print("Register Python signal handler: %r" % sig)
+        signal.signal(sig, idle_handler)
+        GLib.idle_add(install_glib_handler, sig, priority=GLib.PRIORITY_HIGH)
+
+
 if __name__ == '__main__':
     args = argparser()
 
@@ -124,4 +166,5 @@ if __name__ == '__main__':
         load_plugins(root)
 
     tray = TrayIcon(root)
+    InitSignal(root)
     Gtk.main()
