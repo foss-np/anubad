ce8d3920a1d99d7e4a6e1fc68b3b1ade70b61dc8
rho <rho.rhoit@gmail.com>
2016-06-20 10:18:42 +0545

    plugins for linux environment

---
+++ plugins/linux.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""
+plugins specific to linux environment
+"""
+
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Notify', '0.7')
+gi.require_version('Keybinder', '3.0')
+
+from gi.repository import Gtk, Keybinder, Notify
+
+notification = Notify.Notification()
+
+def grab_notify(apps, *args):
+    clip = check_output(["xclip", "-o"], universal_newlines=True)
+    if clip is None: return
+    query = clip.strip().lower()
+    apps.root.search_entry.set_text(clip)
+    apps.root.search_and_reflect()
+    notifier(apps)
+
+
+def notifier(app, *args):
+    buffer = app.root.viewer.textbuffer
+
+    insert = buffer.get_insert()
+    beg    = buffer.get_start_iter()
+    end    = buffer.get_iter_at_mark(insert)
+
+    text   = buffer.get_text(beg, end, True)
+    f_text = buffer.register_serialize_tagset(text)
+    # buffer.serialize(buffer, format, beg, end)
+    # print(text)
+    # print(f_text)
+
+    notification.update(text)
+    #notification.update(text, body, icon)
+    # notification.add_action(action, label, callback, *user_data)
+    notification.show()
+
+    xcowsay = app.plugins['xcowsay']
+    xcowsay.popup(text)
+
+
+def plugin_main(app, fullpath):
+    Keybinder.init()
+    Keybinder.bind("<Ctrl><Alt>v", lambda *a: grab_notify(app.root))
+    Keybinder.bind("<Ctrl><Alt>m", lambda *a: notifier(app))
+    Notify.init("anubad")
+    notification.set_icon_from_pixbuf(app.pixbuf_logo)
+    return True
