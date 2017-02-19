1bcc6b496e3a17bd765a1290d4958d0ae87d0bf1
Milan Thapa <milans.thapa78@gmail.com>
2017-02-19 16:39:44 +0545

    dump ready to export

--- src/dump.py
+++ src/dump.py
@@ -13,7 +13,7 @@ core.load_from_config(cnf)
 with open("gloss.dump.json", 'w') as fp:
     for instance in core.Glossary.instances.values():
 
-        liststore, invert = instance['main.tra']
+        liststore, invert = instance['food.tra']
         clean = []
 
         for word, (ID, info) in liststore.items():
@@ -22,29 +22,37 @@ with open("gloss.dump.json", 'w') as fp:
             empty_info = dict()
             empty_info['w'] = word
             for tag, value in info.items():
+                # print("tag=",tag," value=", value)
+
+                # extracting the scientific name
+                if tag == "sci":
+                  empty_info['sci'] = value
+
                 if not tag[-1].isdigit(): continue
                 pos = empty_info.get(tag[:-1], [])
                 pos_obj = {
                     "tr": value
                 }
                 for hashtag, p in info['#'].items():
-                    if tag != p: continue
-                    pos.get("#",[]).append(hashtag)
+                    if tag not in p: continue
+                    pos_obj["#"] = hashtag
+
+                # tag= t  value= {'विन्ड': ['n1'], 'वाइड': ['v1']}
+                for t, p in info['t'].items():
+                    if tag not in p: continue 
+                    pos_obj["t"] = t
 
                 pos.append(pos_obj)
+                # print(pos)
                 empty_info[tag[:-1]] = pos
                 
-                # print(tag, value)
-
-            
+            # handling the global tags
             for hashtag, p in info['#'].items():
                 # print(hashtag, p)
                 if "" not in p: continue
                 gtag = empty_info.get("#",[])
                 gtag.append(hashtag)
                 empty_info["#"] = gtag
-                    
-            # print(info)
             print(empty_info)
             # exit()
 
